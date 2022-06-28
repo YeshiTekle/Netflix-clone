dc39d82440e9ae09ec6dad8762d74ca4baf6101c
YeshiTekle <104454995+YeshiTekle@users.noreply.github.com>
2022-06-27 21:33:14 -0400

    Update Row.js

--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 import YouTube from "react-youtube"
-import movieTrailer from "movie-trailer"
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -18,7 +18,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
   const opts = {
-    heighr: "390",
+    height: "390",
     width: "100%",
     playerVars: {
       autoplay: 1,
